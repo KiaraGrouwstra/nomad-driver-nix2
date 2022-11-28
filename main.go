8977e5122acdb41e5e04f9d32338baf27fda6f49
Alex Auvolat <alex@adnab.me>
2022-11-28 12:24:07 +0100

    rename things

--- main.go
+++ main.go
@@ -1,8 +1,7 @@
 package main
 
 import (
-	// TODO: update the path below to match your own repository
-	"github.com/hashicorp/nomad-skeleton-driver-plugin/hello"
+	exec2 "github.com/Alexis211/nomad-driver-exec2/exec2"
 
 	"github.com/hashicorp/go-hclog"
 	"github.com/hashicorp/nomad/plugins"
@@ -15,5 +14,5 @@ func main() {
 
 // factory returns a new instance of a nomad driver plugin
 func factory(log hclog.Logger) interface{} {
-	return hello.NewPlugin(log)
+	return exec2.NewPlugin(log)
 }
